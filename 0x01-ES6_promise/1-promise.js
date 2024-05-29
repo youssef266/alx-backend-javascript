035c6a550be75b9131dce31502679c1dcd6f6a4d
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-05-29 23:49:13 +0300

    Alx

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,6 @@
-export default function getFullResponseFromAPI(success) {
-  const arr = new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: "Success" });
-    } else {
-      reject(new Error("The fake API is not working currently"));
-    }
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(new Error('The fake API is not working currently'));
   });
-  return arr;
 }
