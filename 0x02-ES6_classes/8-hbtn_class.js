316767103c7c8948e85ff8d50ec6993505fe30fe
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-05-30 17:40:43 +0300

    alx

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,32 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(size) {
+    this._size = size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(location) {
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return null;
+  }
+}
