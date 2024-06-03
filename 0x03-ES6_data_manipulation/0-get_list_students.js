2280d63bdd1f001ff4f9b1a73ae174498060fabf
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-06-03 23:55:56 +0300

    alx

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,13 @@
-function getListStudents() {
-    return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-    ];
+export default function getListStudents() {
+  const createStudent = (id, firstName, location) => ({
+    id,
+    firstName,
+    location,
+  });
+
+  return [
+    createStudent(1, 'Guillaume', 'San Francisco'),
+    createStudent(2, 'James', 'Columbia'),
+    createStudent(5, 'Serena', 'San Francisco'),
+  ];
 }
